2bfc8e70c3b50d9fa37ef5453b71489da98b62ca
Nick Markham <nickmarkham@gmail.com>
2016-01-30 10:54:13 +0800

    update 30-1-15
    to include fix to reset and new lowercase function

--- assignment/starter_code/js/script.js
+++ assignment/starter_code/js/script.js
@@ -4,21 +4,24 @@ $(document).ready(function() {
   event.preventDefault();
 
     var city = $('#city-type').val();
-    $('#city-input').val('');
+    $('#city-type').val('');
+    city = city.toLowerCase().trim();
 
-        if (city == "New York" || city =="New York City" || city == "NYC"){
-          $('body').attr('class','nyc');
+// .trim reduces white space - look into this further
 
-      } else if (city == "San Francisco" || city == "SF" || city =="Bay Area"){
+        if (city === "new York" || city ==="new york city" || city === "nyc"){
+          $('body').attr("class","nyc");
+
+      } else if (city === "san francisco" || city === "sf" || city ==="bay area"){
           $('body').attr('class','sf');
 
-      } else if (city == "Los Angeles" || city =="LA" || city == "LAX") {
+      } else if (city === "los angeles" || city ==="la" || city === "lax") {
           $('body').attr('class','la');
 
-      } else if (city == "Austin" || city =="ATX") {
+      } else if (city === "austin" || city ==="atx") {
          $('body').attr('class','austin');
 
-      } else if (city == "Sydney" || city =="SYD") {
+      } else if (city === "sydney" || city ==="syd") {
          $('body').attr('class','sydney');
 
 // Will, how would i make it so if another value is entered that does not match the options, it gives a pop up?
@@ -28,12 +31,11 @@ $(document).ready(function() {
       }
 
 // Will, i found these three examples of how to reset a form after clicked. But i cant for the life of me figure out
-// which one is best used to reset and where in the script it should sit. Can you give a pointer? Thanks
+// Which one is best used to reset and where in the script it should sit. Can you give a pointer? Thanks
 
     // $('#city-type').trigger("reset");
     // $("#city-type")[0].reset();
-    // $('#city-type').children('input').val('')
-
+    // $('#city-type').val('') - turns out this is the correct one. 
 
   });
 });
